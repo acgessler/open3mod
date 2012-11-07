874904ed855a6d2926410835c210750111c6ce9e
acg@GANDALF.vaih.selfnet.de <acg@GANDALF.vaih.selfnet.de>
2012-11-07 18:55:37 +0100

    - compute bounding box for loaded scenes.

---
+++ open3mod/AssimpToOpenTk.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assimp;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace open3mod
+{
+    /// <summary>
+    /// Static utility functions to convert assimp math structures to their OpenTK counterparts
+    /// </summary>
+    public static class AssimpToOpenTk
+    {
+        public static Matrix4 FromMatrix(Matrix4x4 mat)
+        {
+            var m = new Matrix4
+            {
+                M11 = mat.A1,
+                M12 = mat.A2,
+                M13 = mat.A3,
+                M14 = mat.A4,
+                M21 = mat.B1,
+                M22 = mat.B2,
+                M23 = mat.B3,
+                M24 = mat.B4,
+                M31 = mat.C1,
+                M32 = mat.C2,
+                M33 = mat.C3,
+                M34 = mat.C4,
+                M41 = mat.D1,
+                M42 = mat.D2,
+                M43 = mat.D3,
+                M44 = mat.D4
+            };
+            return m;
+        }
+
+        public static Vector3 FromVector(Vector3D vec)
+        {
+            Vector3 v;
+            v.X = vec.X;
+            v.Y = vec.Y;
+            v.Z = vec.Z;
+            return v;
+        }
+
+        public static Color4 FromColor(Color4D color)
+        {
+            Color4 c;
+            c.R = color.R;
+            c.G = color.G;
+            c.B = color.B;
+            c.A = color.A;
+            return c;
+        }
+    }
+}
--- open3mod/Scene.cs
+++ open3mod/Scene.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Assimp;
 using Assimp.Configs;
+using OpenTK;
 
 namespace open3mod
 {
@@ -20,6 +21,9 @@ namespace open3mod
     public class Scene
     {
         private readonly Assimp.Scene _raw;
+        private Vector3 _sceneCenter;
+        private Vector3 _sceneMin;
+        private Vector3 _sceneMax;
 
         /// <summary>
         /// Obtain the "raw" scene data as imported by Assimp
@@ -27,6 +31,11 @@ namespace open3mod
         public Assimp.Scene Raw
         {
             get { return _raw; }
+        }
+
+        public Vector3 SceneCenter
+        {
+            get { return _sceneCenter; }
         }
 
 
@@ -48,6 +57,75 @@ namespace open3mod
             //  - request lots of post processing steps, the details of which
             //    can be found in the TargetRealTimeMaximumQuality docs.
             _raw = imp.ImportFile(file, PostProcessPreset.TargetRealTimeMaximumQuality);
-        }      
+            if (_raw == null)
+            {
+                throw new Exception("failed to read file: " + file);
+            }
+
+            // compute a bounding box (AABB) for the scene we just loaded
+            ComputeBoundingBox();
+        } 
+     
+
+
+        public void Update(double delta)
+        {
+            
+        }
+
+
+        public void Render()
+        {
+            
+        }
+
+
+
+        private void ComputeBoundingBox()
+        {
+            _sceneMin = new Vector3(1e10f, 1e10f, 1e10f);
+            _sceneMax = new Vector3(-1e10f, -1e10f, -1e10f);
+            Matrix4 identity = Matrix4.Identity;
+
+            ComputeBoundingBox(_raw.RootNode, ref _sceneMin, ref _sceneMax, ref identity);
+
+            _sceneCenter.X = (_sceneMin.X + _sceneMax.X) / 2.0f;
+            _sceneCenter.Y = (_sceneMin.Y + _sceneMax.Y) / 2.0f;
+            _sceneCenter.Z = (_sceneMin.Z + _sceneMax.Z) / 2.0f;
+        }
+
+
+        private void ComputeBoundingBox(Node node, ref Vector3 min, ref Vector3 max, ref Matrix4 trafo)
+        {
+            Matrix4 prev = trafo;
+            trafo = Matrix4.Mult(prev, AssimpToOpenTk.FromMatrix(node.Transform));
+
+            if (node.HasMeshes)
+            {
+                foreach (int index in node.MeshIndices)
+                {
+                    Mesh mesh = _raw.Meshes[index];
+                    for (int i = 0; i < mesh.VertexCount; i++)
+                    {
+                        Vector3 tmp = AssimpToOpenTk.FromVector(mesh.Vertices[i]);
+                        Vector3.Transform(ref tmp, ref trafo, out tmp);
+
+                        min.X = Math.Min(min.X, tmp.X);
+                        min.Y = Math.Min(min.Y, tmp.Y);
+                        min.Z = Math.Min(min.Z, tmp.Z);
+
+                        max.X = Math.Max(max.X, tmp.X);
+                        max.Y = Math.Max(max.Y, tmp.Y);
+                        max.Z = Math.Max(max.Z, tmp.Z);
+                    }
+                }
+            }
+
+            for (int i = 0; i < node.ChildCount; i++)
+            {
+                ComputeBoundingBox(node.Children[i], ref min, ref max, ref trafo);
+            }
+            trafo = prev;
+        }       
     }
 }
