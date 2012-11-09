853dc6178b427447bfd1749d86b6b298aee0e62f
acg@GANDALF.vaih.selfnet.de <acg@GANDALF.vaih.selfnet.de>
2012-11-09 20:30:21 +0100

    - initial work on log viewer.

---
+++ open3mod/LogStore.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace open3mod
+{
+    /// <summary>
+    /// Stores a list of logging messages obtained from assimp
+    /// Log messages are intercepted in this fashion to enable arbitrary
+    /// filtering in the UI.
+    /// </summary>
+    public class LogStore
+    {
+
+        /// <summary>
+        /// Enumeration of logging levels. Category.System is for logging
+        /// messsages produced by viewer code while all other categories
+        /// are strictly taken from assimp.
+        /// </summary>
+        public enum Category
+        {
+            Info = 0,
+            Warn,
+            Error,
+            Debug,
+
+            System
+        }
+
+        /// <summary>
+        /// Represents a single logging entry along with a timestamp
+        /// </summary>
+        public struct Entry
+        {
+            public Category cat;
+            public string message;
+            public double time;
+        }
+
+
+
+        public List<Entry> Messages
+        {
+            get { return _messages; }
+        }
+
+
+        /// <summary>
+        /// Construct a fresh store for log messages
+        /// </summary>
+        /// <param name="capacity">Number of entries to be expected</param>
+        public LogStore(int capacity = 200)
+        {
+            _messages = new List<Entry>(capacity);
+        }
+
+
+        public void Add(Category cat, string message, double time)
+        {
+            _messages.Add(new Entry() {cat = cat, message = message, time = time});
+        }
+
+
+
+        private readonly List<Entry> _messages;
+    }
+}
+
---
+++ open3mod/LogViewer.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace open3mod
+{
+    partial class LogViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.checkBoxFilterError = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxFilterWarning = new System.Windows.Forms.CheckBox();
+            this.checkBoxFilterInformation = new System.Windows.Forms.CheckBox();
+            this.checkBoxFilterVerbose = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox.Location = new System.Drawing.Point(0, 142);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBox.Size = new System.Drawing.Size(821, 429);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            // 
+            // checkBoxFilterError
+            // 
+            this.checkBoxFilterError.AutoSize = true;
+            this.checkBoxFilterError.Checked = true;
+            this.checkBoxFilterError.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxFilterError.Location = new System.Drawing.Point(21, 30);
+            this.checkBoxFilterError.Name = "checkBoxFilterError";
+            this.checkBoxFilterError.Size = new System.Drawing.Size(53, 17);
+            this.checkBoxFilterError.TabIndex = 1;
+            this.checkBoxFilterError.Text = "Errors";
+            this.checkBoxFilterError.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxFilterVerbose);
+            this.groupBox1.Controls.Add(this.checkBoxFilterInformation);
+            this.groupBox1.Controls.Add(this.checkBoxFilterWarning);
+            this.groupBox1.Controls.Add(this.checkBoxFilterError);
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(201, 92);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter";
+            // 
+            // checkBoxFilterWarning
+            // 
+            this.checkBoxFilterWarning.AutoSize = true;
+            this.checkBoxFilterWarning.Checked = true;
+            this.checkBoxFilterWarning.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxFilterWarning.Location = new System.Drawing.Point(21, 53);
+            this.checkBoxFilterWarning.Name = "checkBoxFilterWarning";
+            this.checkBoxFilterWarning.Size = new System.Drawing.Size(71, 17);
+            this.checkBoxFilterWarning.TabIndex = 2;
+            this.checkBoxFilterWarning.Text = "Warnings";
+            this.checkBoxFilterWarning.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFilterInformation
+            // 
+            this.checkBoxFilterInformation.AutoSize = true;
+            this.checkBoxFilterInformation.Checked = true;
+            this.checkBoxFilterInformation.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxFilterInformation.Location = new System.Drawing.Point(112, 30);
+            this.checkBoxFilterInformation.Name = "checkBoxFilterInformation";
+            this.checkBoxFilterInformation.Size = new System.Drawing.Size(78, 17);
+            this.checkBoxFilterInformation.TabIndex = 3;
+            this.checkBoxFilterInformation.Text = "Information";
+            this.checkBoxFilterInformation.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFilterVerbose
+            // 
+            this.checkBoxFilterVerbose.AutoSize = true;
+            this.checkBoxFilterVerbose.Checked = true;
+            this.checkBoxFilterVerbose.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxFilterVerbose.Location = new System.Drawing.Point(112, 53);
+            this.checkBoxFilterVerbose.Name = "checkBoxFilterVerbose";
+            this.checkBoxFilterVerbose.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxFilterVerbose.TabIndex = 3;
+            this.checkBoxFilterVerbose.Text = "Verbose";
+            this.checkBoxFilterVerbose.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Clear All";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // LogViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 592);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox);
+            this.Name = "LogViewer";
+            this.Text = "Log File Viewer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.CheckBox checkBoxFilterError;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBoxFilterVerbose;
+        private System.Windows.Forms.CheckBox checkBoxFilterInformation;
+        private System.Windows.Forms.CheckBox checkBoxFilterWarning;
+        private System.Windows.Forms.Button button1;
+    }
+}
