b8c57cf377394fd795a594fcafd7ecaf7b53c3ac
Alexander Christoph Gessler <none@none>
2013-01-07 04:13:06 +0100

    - implement fifo texture queue for async loading.

--- open3mod/Program.cs
+++ open3mod/Program.cs
@@ -17,6 +17,8 @@ namespace open3mod
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
+
+            TextureQueue.Terminate();
         }
     }
 }
--- open3mod/TextureQueue.cs
+++ open3mod/TextureQueue.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace open3mod
@@ -19,10 +20,83 @@ namespace open3mod
     {
         public delegate void CompletionCallback(string file, Image image);
 
+        private struct Task
+        {
+            public readonly string File;
+            public readonly CompletionCallback Callback;
+
+            public Task(string file, CompletionCallback callback) : this()
+            {
+                File = file;
+                Callback = callback;
+            }
+        }
+
+        private static Thread _thread;
+        private static readonly Queue<Task> Queue = new Queue<Task>();
+        private static readonly AutoResetEvent Event = new AutoResetEvent(false);
+
 
         public static void Enqueue(string file, CompletionCallback callback)
         {
-            
+            if (_thread == null)
+            {
+                StartThread();
+            }
+
+            lock (Queue) {
+                Queue.Enqueue(new Task(file, callback));
+            }
+            Event.Set();
         }
+
+        public static void Terminate()
+        {
+            if (_thread == null || !_thread.IsAlive)
+            {
+                return;
+            }
+
+            _thread.Interrupt();
+            // note that completion callbacks may still be called
+            // after this point. With the current implementation
+            // of Texture it doesn't matter, though.
+        }
+
+        private static void StartThread()
+        {
+            _thread = new Thread(ThreadProc);
+        }
+
+        private static void ThreadProc()
+        {
+            try
+            {
+                while (true)
+                {
+                    bool empty;
+                    lock (Queue) {
+                        empty = Queue.Count == 0;
+                    }
+                    if (empty)
+                    {
+                        Event.WaitOne();
+                        continue;
+                    }
+
+                    Task task;
+                    lock (Queue) {
+                        task = Queue.Dequeue();
+                    }
+
+                    // XXX support more file formats (such as dds, tga ..)
+                    task.Callback(task.File, Image.FromFile(task.File));
+                }
+            }
+            catch(ThreadInterruptedException)
+            {
+                
+            }
+        }       
     }
 }
