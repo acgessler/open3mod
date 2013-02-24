d24ba6d56126a8a60aa8fd86cccdc1bd1f7e3c53
Alexander Gessler <alexander.gessler@gmx.net>
2013-02-24 14:43:25 +0100

    - add license headers to all files.

--- open3mod/MaterialInspectionView.cs
+++ open3mod/MaterialInspectionView.cs
@@ -1,4 +1,25 @@
-﻿using System;
+///////////////////////////////////////////////////////////////////////////////////
+// Open 3D Model Viewer (open3mod) (v0.1)
+// [MaterialInspectionView.cs]
+// (c) 2012-2013, Alexander C. Gessler
+//
+// Licensed under the terms and conditions of the 3-clause BSD license. See
+// the LICENSE file in the root folder of the repository for the details.
+//
+// HIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///////////////////////////////////////////////////////////////////////////////////
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,3 +41,5 @@ namespace open3mod
         }
     }
 }
+
+/* vi: set shiftwidth=4 tabstop=4: */ 
