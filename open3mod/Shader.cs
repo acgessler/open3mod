30757c4776570339545c0ccebbcebcadbff700e8
Alexander Gessler <alexander.gessler@gmx.net>
2013-03-23 12:22:33 +0100

    - add Shader utility class.

---
+++ open3mod/Shader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace open3mod
+{
+    /// <summary>
+    /// Represents a shader program that consists of a vertex and a fragment
+    /// shader stage.
+    /// </summary>
+    public class Shader
+    {
+    }
+}
