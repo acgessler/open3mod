9a536f38c562ea400e135f6b3a4a73db01439a55
Alexander Gessler <alexander.gessler@gmx.net>
2013-04-07 19:55:54 +0200

    - forgot to fix some default values.

--- open3mod/CoreSettings.Designer.cs
+++ open3mod/CoreSettings.Designer.cs
@@ -72,7 +72,7 @@ namespace CoreSettings {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("50")]
+        [global::System.Configuration.DefaultSettingValueAttribute("-1")]
         public int InspectorSplitterPos {
             get {
                 return ((int)(this["InspectorSplitterPos"]));
