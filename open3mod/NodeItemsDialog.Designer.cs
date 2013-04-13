aa81abc06a8a49e6b49d5b113639cee6976797db
Alexander Gessler <alexander.gessler@gmx.net>
2013-04-13 19:38:10 +0200

    - NodeItemsDialog: show node statistics.

--- open3mod/NodeItemsDialog.Designer.cs
+++ open3mod/NodeItemsDialog.Designer.cs
@@ -31,16 +31,26 @@
             this.checkBoxShowAnimated = new System.Windows.Forms.CheckBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.labelChildrenDirect = new System.Windows.Forms.Label();
+            this.labelChildrenTotal = new System.Windows.Forms.Label();
+            this.labelMeshesDirect = new System.Windows.Forms.Label();
+            this.labelMeshesTotal = new System.Windows.Forms.Label();
             this.trafoMatrixViewControlGlobal = new open3mod.TrafoMatrixViewControl();
             this.trafoMatrixViewControlLocal = new open3mod.TrafoMatrixViewControl();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // checkBoxShowAnimated
             // 
             this.checkBoxShowAnimated.AutoSize = true;
-            this.checkBoxShowAnimated.Location = new System.Drawing.Point(18, 12);
+            this.checkBoxShowAnimated.Location = new System.Drawing.Point(247, 570);
             this.checkBoxShowAnimated.Name = "checkBoxShowAnimated";
             this.checkBoxShowAnimated.Size = new System.Drawing.Size(133, 17);
             this.checkBoxShowAnimated.TabIndex = 0;
@@ -50,9 +60,9 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.trafoMatrixViewControlLocal);
-            this.groupBox1.Location = new System.Drawing.Point(12, 55);
+            this.groupBox1.Location = new System.Drawing.Point(12, 113);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(368, 208);
+            this.groupBox1.Size = new System.Drawing.Size(368, 210);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Node Local Transformation";
@@ -60,12 +70,105 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.trafoMatrixViewControlGlobal);
-            this.groupBox2.Location = new System.Drawing.Point(12, 284);
+            this.groupBox2.Location = new System.Drawing.Point(12, 342);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(368, 210);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Node Global Transformation";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Children (direct):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Children (total):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(190, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Meshes (owned):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(190, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Meshes (total):";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labelMeshesTotal);
+            this.groupBox3.Controls.Add(this.labelMeshesDirect);
+            this.groupBox3.Controls.Add(this.labelChildrenTotal);
+            this.groupBox3.Controls.Add(this.labelChildrenDirect);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(368, 82);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Statistics";
+            // 
+            // labelChildrenDirect
+            // 
+            this.labelChildrenDirect.AutoSize = true;
+            this.labelChildrenDirect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelChildrenDirect.Location = new System.Drawing.Point(105, 28);
+            this.labelChildrenDirect.Name = "labelChildrenDirect";
+            this.labelChildrenDirect.Size = new System.Drawing.Size(52, 13);
+            this.labelChildrenDirect.TabIndex = 8;
+            this.labelChildrenDirect.Text = "<numC>";
+            // 
+            // labelChildrenTotal
+            // 
+            this.labelChildrenTotal.AutoSize = true;
+            this.labelChildrenTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelChildrenTotal.Location = new System.Drawing.Point(105, 54);
+            this.labelChildrenTotal.Name = "labelChildrenTotal";
+            this.labelChildrenTotal.Size = new System.Drawing.Size(52, 13);
+            this.labelChildrenTotal.TabIndex = 9;
+            this.labelChildrenTotal.Text = "<numC>";
+            // 
+            // labelMeshesDirect
+            // 
+            this.labelMeshesDirect.AutoSize = true;
+            this.labelMeshesDirect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMeshesDirect.Location = new System.Drawing.Point(278, 28);
+            this.labelMeshesDirect.Name = "labelMeshesDirect";
+            this.labelMeshesDirect.Size = new System.Drawing.Size(52, 13);
+            this.labelMeshesDirect.TabIndex = 10;
+            this.labelMeshesDirect.Text = "<numC>";
+            // 
+            // labelMeshesTotal
+            // 
+            this.labelMeshesTotal.AutoSize = true;
+            this.labelMeshesTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMeshesTotal.Location = new System.Drawing.Point(278, 54);
+            this.labelMeshesTotal.Name = "labelMeshesTotal";
+            this.labelMeshesTotal.Size = new System.Drawing.Size(52, 13);
+            this.labelMeshesTotal.TabIndex = 11;
+            this.labelMeshesTotal.Text = "<numC>";
             // 
             // trafoMatrixViewControlGlobal
             // 
@@ -78,14 +181,15 @@
             // 
             this.trafoMatrixViewControlLocal.Location = new System.Drawing.Point(6, 20);
             this.trafoMatrixViewControlLocal.Name = "trafoMatrixViewControlLocal";
-            this.trafoMatrixViewControlLocal.Size = new System.Drawing.Size(352, 190);
+            this.trafoMatrixViewControlLocal.Size = new System.Drawing.Size(352, 183);
             this.trafoMatrixViewControlLocal.TabIndex = 1;
             // 
             // NodeItemsDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(392, 508);
+            this.ClientSize = new System.Drawing.Size(392, 599);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.checkBoxShowAnimated);
@@ -95,6 +199,8 @@
             this.Text = "<NodeName> Details";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,5 +213,14 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private TrafoMatrixViewControl trafoMatrixViewControlGlobal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label labelChildrenDirect;
+        private System.Windows.Forms.Label labelChildrenTotal;
+        private System.Windows.Forms.Label labelMeshesTotal;
+        private System.Windows.Forms.Label labelMeshesDirect;
     }
 }
--- open3mod/NodeItemsDialog.cs
+++ open3mod/NodeItemsDialog.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,6 +43,30 @@ namespace open3mod
             trafoMatrixViewControlGlobal.SetMatrix(ref mat);
 
             Text = node.Name + " - Details";
+
+            // populate statistics
+            labelMeshesDirect.Text = node.MeshCount.ToString(CultureInfo.InvariantCulture);
+            labelChildrenDirect.Text = node.ChildCount.ToString(CultureInfo.InvariantCulture);
+
+            var meshTotal = 0;
+            var childTotal = 0;
+            CountMeshAndChildrenTotal(node, ref meshTotal, ref childTotal);
+
+            labelMeshesTotal.Text = node.MeshCount.ToString(CultureInfo.InvariantCulture);
+            labelChildrenTotal.Text = node.ChildCount.ToString(CultureInfo.InvariantCulture);
+        }
+
+
+        private void CountMeshAndChildrenTotal(Node node, ref int meshTotal, ref int childTotal)
+        {
+            Debug.Assert(node != null);
+
+            meshTotal += node.MeshCount;
+            childTotal += node.ChildCount;
+            for (var i = 0; i < node.ChildCount; ++i)
+            {
+                CountMeshAndChildrenTotal(node.Children[i], ref meshTotal, ref childTotal);
+            }
         }
     }
 }
