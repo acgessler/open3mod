508138d38eac843fa6220f71a8287b5dec2d2532
Alexander Gessler <alexander.gessler@gmx.net>
2013-04-13 17:10:33 +0200

    - ui draft for transformation matrix viewer.

---
+++ open3mod/TrafoMatrixViewControl.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace open3mod
+{
+    partial class TrafoMatrixViewControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTransX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTransY = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxTransZ = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxScaleZ = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxScaleY = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxScaleX = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxRotZ = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxRotY = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBoxRotX = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.comboBoxRotMode = new System.Windows.Forms.ComboBox();
+            this.textBoxRotW = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Translation:";
+            // 
+            // textBoxTransX
+            // 
+            this.textBoxTransX.Location = new System.Drawing.Point(100, 27);
+            this.textBoxTransX.Name = "textBoxTransX";
+            this.textBoxTransX.Size = new System.Drawing.Size(64, 20);
+            this.textBoxTransX.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(178, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y";
+            // 
+            // textBoxTransY
+            // 
+            this.textBoxTransY.Location = new System.Drawing.Point(194, 27);
+            this.textBoxTransY.Name = "textBoxTransY";
+            this.textBoxTransY.Size = new System.Drawing.Size(64, 20);
+            this.textBoxTransY.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(271, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Z";
+            // 
+            // textBoxTransZ
+            // 
+            this.textBoxTransZ.Location = new System.Drawing.Point(287, 27);
+            this.textBoxTransZ.Name = "textBoxTransZ";
+            this.textBoxTransZ.Size = new System.Drawing.Size(64, 20);
+            this.textBoxTransZ.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(32, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Scaling:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(271, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Z";
+            // 
+            // textBoxScaleZ
+            // 
+            this.textBoxScaleZ.Location = new System.Drawing.Point(287, 53);
+            this.textBoxScaleZ.Name = "textBoxScaleZ";
+            this.textBoxScaleZ.Size = new System.Drawing.Size(64, 20);
+            this.textBoxScaleZ.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(178, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Y";
+            // 
+            // textBoxScaleY
+            // 
+            this.textBoxScaleY.Location = new System.Drawing.Point(194, 53);
+            this.textBoxScaleY.Name = "textBoxScaleY";
+            this.textBoxScaleY.Size = new System.Drawing.Size(64, 20);
+            this.textBoxScaleY.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(84, 56);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "X";
+            // 
+            // textBoxScaleX
+            // 
+            this.textBoxScaleX.Location = new System.Drawing.Point(100, 53);
+            this.textBoxScaleX.Name = "textBoxScaleX";
+            this.textBoxScaleX.Size = new System.Drawing.Size(64, 20);
+            this.textBoxScaleX.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(271, 141);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(14, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Z";
+            // 
+            // textBoxRotZ
+            // 
+            this.textBoxRotZ.Location = new System.Drawing.Point(287, 138);
+            this.textBoxRotZ.Name = "textBoxRotZ";
+            this.textBoxRotZ.Size = new System.Drawing.Size(64, 20);
+            this.textBoxRotZ.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(178, 141);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(14, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Y";
+            // 
+            // textBoxRotY
+            // 
+            this.textBoxRotY.Location = new System.Drawing.Point(194, 138);
+            this.textBoxRotY.Name = "textBoxRotY";
+            this.textBoxRotY.Size = new System.Drawing.Size(64, 20);
+            this.textBoxRotY.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(84, 141);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(14, 13);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "X";
+            // 
+            // textBoxRotX
+            // 
+            this.textBoxRotX.Location = new System.Drawing.Point(100, 138);
+            this.textBoxRotX.Name = "textBoxRotX";
+            this.textBoxRotX.Size = new System.Drawing.Size(64, 20);
+            this.textBoxRotX.TabIndex = 15;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(26, 141);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(59, 13);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Rotation:";
+            // 
+            // comboBoxRotMode
+            // 
+            this.comboBoxRotMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxRotMode.FormattingEnabled = true;
+            this.comboBoxRotMode.Items.AddRange(new object[] {
+            "Euler XYZ (degrees)",
+            "Euler XYZ (radians)",
+            "Unit Quaternion"});
+            this.comboBoxRotMode.Location = new System.Drawing.Point(100, 111);
+            this.comboBoxRotMode.Name = "comboBoxRotMode";
+            this.comboBoxRotMode.Size = new System.Drawing.Size(158, 21);
+            this.comboBoxRotMode.TabIndex = 21;
+            // 
+            // textBoxRotW
+            // 
+            this.textBoxRotW.Location = new System.Drawing.Point(100, 164);
+            this.textBoxRotW.Name = "textBoxRotW";
+            this.textBoxRotW.Size = new System.Drawing.Size(64, 20);
+            this.textBoxRotW.TabIndex = 22;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(80, 171);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(18, 13);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "W";
+            // 
+            // TrafoMatrixViewControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBoxRotW);
+            this.Controls.Add(this.comboBoxRotMode);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxRotZ);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxRotY);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBoxRotX);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxScaleZ);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxScaleY);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxScaleX);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxTransZ);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxTransY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxTransX);
+            this.Controls.Add(this.label1);
+            this.Name = "TrafoMatrixViewControl";
+            this.Size = new System.Drawing.Size(381, 199);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTransX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTransY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxTransZ;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxScaleZ;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxScaleY;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxScaleX;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxRotZ;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxRotY;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBoxRotX;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox comboBoxRotMode;
+        private System.Windows.Forms.TextBox textBoxRotW;
+        private System.Windows.Forms.Label label13;
+    }
+}
