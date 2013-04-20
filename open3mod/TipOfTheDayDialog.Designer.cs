d2795e1441f42e0be1f59fe47298072da7faae7d
Alexander Gessler <alexander.gessler@gmx.net>
2013-04-20 23:28:59 +0200

    - draft "Tip of the Day" dialog.

---
+++ open3mod/TipOfTheDayDialog.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace open3mod
+{
+    partial class TipOfTheDayDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.checkBoxDoNotShowAgain = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Previous";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOk.Location = new System.Drawing.Point(389, 176);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(100, 23);
+            this.buttonOk.TabIndex = 2;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDoNotShowAgain
+            // 
+            this.checkBoxDoNotShowAgain.AutoSize = true;
+            this.checkBoxDoNotShowAgain.Location = new System.Drawing.Point(268, 180);
+            this.checkBoxDoNotShowAgain.Name = "checkBoxDoNotShowAgain";
+            this.checkBoxDoNotShowAgain.Size = new System.Drawing.Size(115, 17);
+            this.checkBoxDoNotShowAgain.TabIndex = 3;
+            this.checkBoxDoNotShowAgain.Text = "Do not show again";
+            this.checkBoxDoNotShowAgain.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(502, 160);
+            this.panel1.TabIndex = 4;
+            // 
+            // TipOfTheDayDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 212);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBoxDoNotShowAgain);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "TipOfTheDayDialog";
+            this.Text = "Tip of the Day";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.CheckBox checkBoxDoNotShowAgain;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
