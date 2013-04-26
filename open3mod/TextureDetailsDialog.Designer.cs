4b29d478daa89ced4999cc0f55a6c31810a74877
Alexander Gessler <alexander.gessler@gmx.net>
2013-04-26 02:10:46 +0200

    - draft TextureDetailsDialog

---
+++ open3mod/TextureDetailsDialog.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace open3mod
+{
+    partial class TextureDetailsDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBoxZoom = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.checkBoxHasAlpha = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(687, 584);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBoxZoom
+            // 
+            this.comboBoxZoom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxZoom.FormattingEnabled = true;
+            this.comboBoxZoom.Location = new System.Drawing.Point(556, 14);
+            this.comboBoxZoom.Name = "comboBoxZoom";
+            this.comboBoxZoom.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxZoom.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(514, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Zoom";
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(12, 17);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(101, 13);
+            this.labelInfo.TabIndex = 3;
+            this.labelInfo.Text = "Size: <W> x <H> px";
+            // 
+            // checkBoxHasAlpha
+            // 
+            this.checkBoxHasAlpha.AutoCheck = false;
+            this.checkBoxHasAlpha.AutoSize = true;
+            this.checkBoxHasAlpha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBoxHasAlpha.Location = new System.Drawing.Point(137, 15);
+            this.checkBoxHasAlpha.Name = "checkBoxHasAlpha";
+            this.checkBoxHasAlpha.Size = new System.Drawing.Size(92, 17);
+            this.checkBoxHasAlpha.TabIndex = 4;
+            this.checkBoxHasAlpha.Text = "Alpha Channel";
+            this.checkBoxHasAlpha.UseVisualStyleBackColor = true;
+            // 
+            // TextureDetailsDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 630);
+            this.Controls.Add(this.checkBoxHasAlpha);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxZoom);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "TextureDetailsDialog";
+            this.Text = "<TexName> - Details";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBoxZoom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.CheckBox checkBoxHasAlpha;
+    }
+}
