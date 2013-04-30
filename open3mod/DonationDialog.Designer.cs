6fdf1a709b2d415cddde59d9f95a45d5d39ec045
Alexander Gessler <alexander.gessler@gmx.net>
2013-04-30 12:14:55 +0200

    - add pictures to DonationDialog.

--- open3mod/DonationDialog.Designer.cs
+++ open3mod/DonationDialog.Designer.cs
@@ -33,22 +33,26 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(121, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(399, 78);
+            this.label1.Size = new System.Drawing.Size(378, 91);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(137, 90);
+            this.button1.Location = new System.Drawing.Point(234, 100);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Don\'t show again";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,9 +60,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(15, 90);
+            this.button2.Location = new System.Drawing.Point(124, 100);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.Size = new System.Drawing.Size(104, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Ask again ";
             this.button2.UseVisualStyleBackColor = true;
@@ -66,19 +70,41 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(295, 90);
+            this.button3.Location = new System.Drawing.Point(383, 100);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(116, 23);
-            this.button3.TabIndex = 3;
+            this.button3.TabIndex = 0;
             this.button3.Text = "Donate";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.Donate);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 129);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(514, 9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 129);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
             // 
             // DonationDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(425, 129);
+            this.ClientSize = new System.Drawing.Size(620, 145);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -87,6 +113,8 @@
             this.Name = "DonationDialog";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Your friendly donation reminder";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,6 +126,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
 
     }
 }
