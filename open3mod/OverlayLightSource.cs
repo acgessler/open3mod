e4df3fcfc99ca0092e5b02d94046786569a9592c
Alexander Gessler <alexander.gessler@gmx.net>
2013-09-16 13:46:43 +0200

    Visualize light source, transform light into viewspace.

---
+++ open3mod/OverlayLightSource.cs
@@ -0,0 +1,58 @@
+﻿///////////////////////////////////////////////////////////////////////////////////
+// Open 3D Model Viewer (open3mod) (v0.1)
+// [OverlayLightSource.cs]
+// (c) 2012-2013, Open3Mod Contributors
+//
+// Licensed under the terms and conditions of the 3-clause BSD license. See
+// the LICENSE file in the root folder of the repository for the details.
+//
+// HIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assimp;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace open3mod
+{
+    public static class OverlayLightSource
+    {
+        public static void DrawLightSource(Vector3 dir)
+        {
+            
+
+            GL.Disable(EnableCap.Lighting);
+            GL.Disable(EnableCap.Texture2D);
+            GL.Enable(EnableCap.ColorMaterial);
+            GL.Disable(EnableCap.CullFace);
+            GL.ColorMaterial(MaterialFace.FrontAndBack, ColorMaterialParameter.AmbientAndDiffuse);
+
+            GL.Begin(BeginMode.Lines);
+
+            GL.Color4(new Color4(1.0f, 0.0f, 0.0f, 1.0f));
+            GL.Vertex3(dir * 2.0f);
+
+            GL.Color4(new Color4(1.0f, 0.0f, 0.0f, 1.0f));
+            GL.Vertex3(dir * 2.1f);
+
+            GL.End();
+        }
+    }
+}
+
+/* vi: set shiftwidth=4 tabstop=4: */
--- open3mod/SceneRendererClassicGl.cs
+++ open3mod/SceneRendererClassicGl.cs
@@ -104,14 +104,21 @@ namespace open3mod
             GL.Enable(EnableCap.Lighting);
             GL.Enable(EnableCap.Light0);
 
+            var view = cam == null ? Matrix4.LookAt(0, 10, 5, 0, 0, 0, 0, 1, 0) : cam.GetView();
+
+            // light direction
             var dir = new Vector3(1,1,0);
             var mat = renderer.LightRotation;
             Vector3.TransformNormal(ref dir, ref mat, out dir);
 
+            Vector3 world_dir;
+            Vector3.TransformNormal(ref dir, ref view, out world_dir);
+            GL.Light(LightName.Light0, LightParameter.Position, new float[] { world_dir.X, world_dir.Y, world_dir.Z, 0 });
+
+            // light color
             var col = new Vector3(1, 1, 1);
             col *= (0.25f + 1.5f * GraphicsSettings.Default.OutputBrightness/100.0f) * 1.5f;
 
-            GL.Light(LightName.Light0, LightParameter.Position, new float[] { dir.X, dir.Y, dir.Z, 0 });
             GL.Light(LightName.Light0, LightParameter.Diffuse, new float[] { col.X, col.Y, col.Z, 1 });
             GL.Light(LightName.Light0, LightParameter.Specular, new float[] { col.X, col.Y, col.Z, 1 });
 
@@ -121,10 +128,14 @@ namespace open3mod
             }
 
             GL.MatrixMode(MatrixMode.Modelview);
-            var lookat = cam == null ? Matrix4.LookAt(0, 10, 5, 0, 0, 0, 0, 1, 0) : cam.GetView();
-            GL.LoadMatrix(ref lookat);
+            GL.LoadMatrix(ref view);
             //var tmp = 1.0f;
-            
+
+            if (flags.HasFlag(RenderFlags.Shaded))
+            {
+                OverlayLightSource.DrawLightSource(dir);
+            }
+
             var tmp = InitposeMax.X - InitposeMin.X;
             tmp = Math.Max(InitposeMax.Y - InitposeMin.Y, tmp);
             tmp = Math.Max(InitposeMax.Z - InitposeMin.Z, tmp);
@@ -141,6 +152,7 @@ namespace open3mod
                 UploadTextures();
             }
 
+            
             GL.PushMatrix();
 
             // Build and cache Gl displaylists and update only when the scene changes.
