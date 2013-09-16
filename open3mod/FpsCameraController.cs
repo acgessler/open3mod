7ef246bb61fd2b954660d653efef4605b7b56574
diiigle <tobias.rittig@t-online.de>
2013-09-16 20:20:51 +0200

    Add draft of FPS Camera controlling

--- open3mod/FpsCameraController.cs
+++ open3mod/FpsCameraController.cs
@@ -34,17 +34,21 @@ namespace open3mod
         private Matrix4 _orientation;
         private Vector3 _translation;
 
-        private static readonly Vector3 StartPosition = new Vector3(0.0f,2.0f,10.0f);
+        private static readonly Vector3 StartPosition = new Vector3(0.0f,1.0f,2.5f);
         private bool _dirty = true;
+        private bool _updateOrientation = true;
         private const float MovementBaseSpeed = 1.0f;
         private const float BaseZoomSpeed = 0.002f;
         private const float RotationSpeed = 0.5f;
+
+        private float _pitchAngle = 0.0f;
+        private float _rollAngle = 0.0f;
+        private float _yawAngle = 0.0f;
 
 
         public FpsCameraController()
         {
             _view = Matrix4.Identity;
-            _orientation = Matrix4.Identity;
             _translation = StartPosition;
 
             UpdateViewMatrix();
@@ -71,11 +75,11 @@ namespace open3mod
         public void MovementKey(float x, float y, float z)
         {
             var v = new Vector3(x, y, z) * MovementBaseSpeed;
-            var o = _orientation;
+            var o = GetOrientation();
 
             // TODO: somehow, the matrix is transposed so the normal TransformVector() API does not work.
             // It seems we messed up with OpenTK's matrix conventions.
-            _translation += v.X * _orientation.Row0.Xyz + v.Y * _orientation.Row1.Xyz + v.Z * _orientation.Row2.Xyz;
+            _translation += v.X * o.Row0.Xyz + v.Y * o.Row1.Xyz + v.Z * o.Row2.Xyz;
             _dirty = true;
         }
 
@@ -90,23 +94,23 @@ namespace open3mod
         {
             if (y != 0)
             {
-                _orientation *= Matrix4.CreateFromAxisAngle(_orientation.Row0.Xyz,
-                    (float)(-y * RotationSpeed * Math.PI / 180.0));
+                _pitchAngle += (float)(-y * RotationSpeed * Math.PI / 180.0);
             }
 
             if (x != 0)
             {
-                _orientation *= Matrix4.CreateFromAxisAngle(
-                    Vector3.UnitY, (float)(-x * RotationSpeed * Math.PI / 180.0));
+                _yawAngle += (float)(-x * RotationSpeed * Math.PI / 180.0);
             }
 
             _dirty = true;
+            _updateOrientation = true;
         }
 
 
         public void Scroll(float z)
         {
-            _translation -= _orientation.Row2.Xyz *z*BaseZoomSpeed;
+            var o = GetOrientation();
+            _translation -= o.Row2.Xyz *z*BaseZoomSpeed;
             _dirty = true;
         }
 
@@ -118,17 +122,34 @@ namespace open3mod
             // =>  view = translation^-1 * orientation^-1
             // =>  view = translation^-1 * orientation^T      ; orientation is ONB
             // where translation^-1 is simply the negated translation vector.
-
-            _view = _orientation;
+            _view = GetOrientation();
+            _view *= Matrix4.CreateFromAxisAngle(_view.Row0.Xyz, _pitchAngle);
             _view.Transpose();
 
             _view = Matrix4.CreateTranslation(-_translation) * _view;
             _dirty = false;
         }
 
+        private Matrix4 GetOrientation()
+        {
+            if (_updateOrientation)
+            {
+                _updateOrientation = false;
+                _orientation = Matrix4.CreateFromAxisAngle(Vector3.UnitY, _yawAngle);
+                _orientation *= Matrix4.CreateFromAxisAngle(_orientation.Row0.Xyz, _pitchAngle);   
+            }
+            return _orientation;
+        }
+
         public void LeapInput(float x, float y, float z, float pitch, float roll, float yaw)
         {
+            Scroll(-z);
+            //TODO parameters in Settings
+            _pitchAngle += pitch * 0.05f;
+            _yawAngle += -yaw * 0.05f;
+            _updateOrientation = true;
 
+            UpdateViewMatrix();
         }
     }
 }
