3173071c60b252843e94af8dfbe267fc44ee10f5
Alexander Gessler <acg@google.com>
2015-03-31 20:01:05 +0200

    Fix null ref access when texture panel is open and you hover over a texture that failed to load.

--- open3mod/TextureDetailsDialog.cs
+++ open3mod/TextureDetailsDialog.cs
@@ -35,12 +35,15 @@ namespace open3mod
             _tex = tex;
             var img = tex.Texture.Image;
 
-            Text = Path.GetFileName(tex.FilePath) + " - Details"; 
+            Text = Path.GetFileName(tex.FilePath) + " - Details";
 
             pictureBox1.Image = img;
-            labelInfo.Text = string.Format("Size: {0} x {1} px", img.Width, img.Height);
-            checkBoxHasAlpha.Checked = tex.Texture.HasAlpha == Texture.AlphaState.HasAlpha;
 
+            if (img != null)
+            {
+                labelInfo.Text = string.Format("Size: {0} x {1} px", img.Width, img.Height);
+            }
+            checkBoxHasAlpha.Checked = tex.Texture.HasAlpha == Texture.AlphaState.HasAlpha;
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
         }
     }
