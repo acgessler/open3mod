9399da0e29d358489e58e97a72f59f90d742d94b
Alexander Gessler <acg@google.com>
2015-04-11 12:22:02 +0200

    Switch ParallelForEachUtility to use the Smart Thread Pool library (https://github.com/amibar/SmartThreadPool).
    Added a trimmed down copy to libs, some fixes to make it .net 3.5 client profile compatible.

---
+++ libs/SmartThreadPool/SmartThreadPool/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Amib.Threading")]
+[assembly: AssemblyDescription("Smart Thread Pool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Amib.Threading")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c764a3de-c4f8-434d-85b5-a09830d1e44f")]
+[assembly: AssemblyVersion("2.2.3.0")]
+
+#if (_PUBLISH)
+[assembly: InternalsVisibleTo("STPTests,PublicKey=00240000048000009400000006020000002400005253413100040000010001004fe3d39add741ba7c8d52cd1eb0d94c7d79060ad956cbaff0e51c1dce94db10356b261778bc1ac3114b3218434da6fcd8416dd5507653809598f7d2afc422099ce4f6b7b0477f18e6c57c727ef2a7ab6ee56e6b4589fe44cb0e25f2875a3c65ab0383ee33c4dd93023f7ce1218bebc8b7a9a1dac878938f5c4f45ea74b6bd8ad")]
+#else
+[assembly: InternalsVisibleTo("STPTests")]
+#endif
+
+
--- open3mod/ParallelForEach.cs
+++ open3mod/ParallelForEach.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using Amib.Threading;
 
 namespace open3mod
 {
@@ -14,13 +15,17 @@ namespace open3mod
     {
         public const int DefaultMinChunkSize = 200;
 
+        private static readonly SmartThreadPool DefaultSmartThreadPool = new SmartThreadPool(1000, 16, 8);
+
         /// <summary>
         /// Break |list| into slices of at least |minChunkSize| and schedule them on a thread pool.
         /// |action| is executed on each slice.
         /// </summary>
         public static void ParallelDo<T>(this IList<T> list, Action<IEnumerable<T>> action,
-            int minChunkSize = DefaultMinChunkSize)
+            int minChunkSize = DefaultMinChunkSize,
+            SmartThreadPool threadPool = null)
         {
+            threadPool = threadPool ?? DefaultSmartThreadPool;
             const int maxHandles = 64; // Maximum for WaitHandle
             var count = list.Count();
             if (count == 0)
@@ -35,31 +40,23 @@ namespace open3mod
             }
             var parallelism = Math.Min(maxHandles, Math.Max(1, count / minChunkSize));
             var effectiveChunkSize = count/parallelism;
-            var resetEvents = new WaitHandle[parallelism];
-            int[] cancelled = {0};
+            var items = new IWorkItemResult[parallelism];
             for (var offset = 0; offset < parallelism; offset++)
             {
                 var start = effectiveChunkSize * offset;
                 var chunk = list.Skip(start).Take(offset == parallelism - 1 ? count - start : effectiveChunkSize);
-                var resetEvent = new ManualResetEvent(false);
-                resetEvents[offset] = resetEvent;
-                ThreadPool.QueueUserWorkItem(
-                    _ =>
-                    {
-                        if (Thread.VolatileRead(ref cancelled[0]) == 0)
-                        {
-                            action(chunk);
-                        }                    
-                        resetEvent.Set();
-                    });
+                items[offset] = threadPool.QueueWorkItem(() => action(chunk));
             }
             try
             {
-                WaitHandle.WaitAll(resetEvents);
+                SmartThreadPool.WaitAll(items);
             }
             catch (ThreadInterruptedException ex)
             {
-                Thread.VolatileWrite(ref cancelled[0], 1);
+                foreach (var item in items)
+                {
+                    item.Cancel();
+                }
                 throw;
             }
         }
@@ -69,7 +66,8 @@ namespace open3mod
         /// |action| is executed on each individual item.
         /// </summary>
         public static void ParallelDo<T>(this IList<T> list, Action<T> action,
-            int minChunkSize = DefaultMinChunkSize)
+            int minChunkSize = DefaultMinChunkSize,
+            SmartThreadPool threadPool = null)
         {
             list.ParallelDo(
                 (chunk) =>
@@ -78,7 +76,7 @@ namespace open3mod
                     {
                         action(elem);
                     }
-                }, minChunkSize);
+                }, minChunkSize, threadPool);
         }
     }
 }
